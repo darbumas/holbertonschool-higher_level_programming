e3f8901387bed13e47f69c74c8754210303e5eb5
darbumas <2314@holbertonschool.com>
2022-01-06 19:57:43 +0000

    format

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in range(len(row)):
-            print("{}".format(row[element]), end="")
-            if element < len(row) - 1:
+            print("{:d}".format(row[element]), end="")
+            if element != len(row) - 1:
                 print(" ", end="")
         print()
