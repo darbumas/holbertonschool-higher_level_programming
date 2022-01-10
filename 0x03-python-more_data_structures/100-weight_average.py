2f66af1efcc1a7136830bae00411083f3d04cab6
darbumas <2314@holbertonschool.com>
2022-01-10 23:25:07 +0000

    code 100

---
+++ 0x03-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        weight_mult, weight_sum = 0, 0
+        for num in my_list:
+            weight_mult += (num[0] * num[1])
+        for num in my_list:
+            weight_sum += num[1]
+        return weight_mult / weight_sum
