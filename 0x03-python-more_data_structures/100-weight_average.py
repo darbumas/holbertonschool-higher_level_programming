c11ce4c02eb33d718bd849b4461137417b26506b
darbumas <2314@holbertonschool.com>
2022-01-10 23:31:45 +0000

    0 return for empty list case

--- 0x03-python-more_data_structures/100-weight_average.py
+++ 0x03-python-more_data_structures/100-weight_average.py
@@ -6,4 +6,6 @@ def weight_average(my_list=[]):
             weight_mult += (num[0] * num[1])
         for num in my_list:
             weight_sum += num[1]
-        return (weight_mult / weight_sum)
+        return weight_mult / weight_sum
+    else:
+        return 0
