5bed5da38693b069c080ec5ba92edcf94decf59e
darbumas <2314@holbertonschool.com>
2022-01-11 20:05:20 +0000

    code #4

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module creates a new class Square"""
+
+
+class Square:
+    """class 'Square' is instatiated with a private instance
+    attribute: '_size"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Private attribute 'size' getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Private attribute 'size' setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Instance method returns the area of a 'Square' object"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """public instance method prints in stdout the square with the
+        the character '#' """
+        if self.__size == 0:
+            print()
+        else:
+            for size1 in range(self.__size):
+                for size2 in range(self.__size):
+                    print('#', end='')
+                print()
