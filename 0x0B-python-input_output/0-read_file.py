8e5ccf86a414c66eb6bff041dece343459418e1b
darbumas <2314@holbertonschool.com>
2022-01-21 19:33:53 +0000

    read each line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Reads from a file and prints content to stdout."""
     with open(filename, mode="r", encoding="utf-8") as MyFile:
-        print(MyFile.read())
+        print(MyFile.read(), end="")
