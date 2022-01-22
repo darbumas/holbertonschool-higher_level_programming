493bde539b4887ccaf5391e20b682ba925b16a8f
darbumas <2314@holbertonschool.com>
2022-01-22 02:44:10 +0000

    add encoding

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,5 +9,5 @@ from isort import file
 
 def save_to_json_file(my_obj, filename):
     """Writes to a file a JSON representation of an object"""
-    with open(filename, mode='w') as MyFile:
+    with open(filename, mode="w", encoding="utf-8") as MyFile:
         return MyFile.write(json.dumps(my_obj))
