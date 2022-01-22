7c0850d2f981bc14b2e14a96511036e413638275
darbumas <2314@holbertonschool.com>
2022-01-22 02:23:36 +0000

    code #4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module defines a ƒ() returns a python data structure represented
+by a JSON string"""
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object represented by a JSON string"""
+    return json.loads(my_str)
