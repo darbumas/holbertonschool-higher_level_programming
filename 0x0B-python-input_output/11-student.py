dbe8cd79549a4a569995affd94bf6230c0b6fd4c
darbumas <2314@holbertonschool.com>
2022-01-22 04:48:50 +0000

    code #11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module creates a class ``Student``"""
+
+
+class Student:
+    """Class with public attributes:
+        1. first_name
+        2. last_name
+        3. age
+        4. to_json(self)
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of the class instance."""
+        if isinstance(attrs, list):
+            my_dict = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    my_dict[i] = getattr(self, i)
+            return my_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of ``Student``."""
+        self.__dict__.update(json)
