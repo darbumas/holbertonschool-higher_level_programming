cdc67c4f61a14681a2eb1996d250424f8dd50632
darbumas <2314@holbertonschool.com>
2022-02-09 19:26:45 +0000

    task #11

---
+++ 0x00-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- lists all records in table 'second_table' with score >= 10
+-- results displayed by score and name and ordered in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC
