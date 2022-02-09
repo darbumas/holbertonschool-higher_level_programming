b399b36a461fa946787683a0460811522dba7eb9
darbumas <2314@holbertonschool.com>
2022-02-09 17:28:52 +0000

    task #4

---
+++ 0x00-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- creates a table 'first_table' in db 'hbtn_0c_0'
+-- <attributes> = id, name -> <values/type> = INT, VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
