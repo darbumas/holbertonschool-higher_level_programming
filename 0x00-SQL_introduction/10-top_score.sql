cacf727fa0cc734530af12468fe8b1e371aa308f
darbumas <2314@holbertonschool.com>
2022-02-09 19:35:42 +0000

    add ';'s & task #12

--- 0x00-SQL_introduction/10-top_score.sql
+++ 0x00-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- lists all records in table 'second_table' of db 'hbtn_0c_c'
 -- results listed by score and name in descending order
-SELECT score, name FROM second_table ORDER BY score DESC
+SELECT score, name FROM second_table ORDER BY score DESC;
