05d89b3f6efa4cde86c1931d36fc231615251dc9
darbumas <2314@holbertonschool.com>
2022-02-09 17:13:44 +0000

    add semicolons @ end of commands

--- 0x00-SQL_introduction/1-create_database_if_missing.sql
+++ 0x00-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- creates a db `hbtn_0c_0`
 -- does not fail if db already exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- 0x00-SQL_introduction/2-remove_database.sql
+++ 0x00-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- deletes db 'hbtn_0c_0'
 -- should not fail if db doesn't exist
-DROP DATABASE IF EXISTS hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0;
