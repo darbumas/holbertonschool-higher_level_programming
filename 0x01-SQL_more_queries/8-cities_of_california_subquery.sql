a7b9152230244ca49dfc82fa6c7c9784772d7d52
darbumas <2314@holbertonschool.com>
2022-02-10 22:41:30 +0000

    task #8

---
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- lists all cities that can be found in db 'hbtn_0d_usa' from California
+-- results are sorted in ascending order by cities.id
+SELECT id, name FROM cities
+WHERE state_id = (
+	SELECT id FROM states
+	WHERE name = 'California')
+ORDER BY id DESC;
