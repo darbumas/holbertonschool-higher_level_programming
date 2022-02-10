66d9fb3b6d881f1a5d40c72d2e9f6990b49244b5
darbumas <2314@holbertonschool.com>
2022-02-10 21:22:50 +0000

    task #2

---
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- creates a database -> 'hbtn_0d_2' and user -> 'user_0d_2'
+-- user has only SELECT privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS
+	'user_0d_2'@'localhost'
+IDENTIFIED BY
+	'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
