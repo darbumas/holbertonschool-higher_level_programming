848cff3c623761ebe3ad8082f29842ce24ba63a9
darbumas <2314@holbertonschool.com>
2022-02-11 04:17:50 +0000

    task #16

---
+++ 0x01-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,13 @@
+-- lists all genres linked to each show in db -> 'hbtn_0d_tvshows'
+-- results display <tv_shows.title> - <tv_genres.name>
+SELECT
+	tv_shows.title, tv_genres.name
+FROM
+	tv_shows
+LEFT JOIN
+	tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN
+	tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY
+	tv_shows.title, tv_genres.name;
+
