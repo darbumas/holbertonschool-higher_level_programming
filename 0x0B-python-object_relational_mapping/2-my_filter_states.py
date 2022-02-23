399cde7540e4429ac86b87c49e111d3ba1d4b149
darbumas <2314@holbertonschool.com>
2022-02-23 23:11:12 +0000

    task #2 with 'format'

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM states WHERE name LIKE BINARY '{argv[4]}' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".
+                format(argv[4]))
     for row in cur.fetchall():
         print(row)
     cur.close()
