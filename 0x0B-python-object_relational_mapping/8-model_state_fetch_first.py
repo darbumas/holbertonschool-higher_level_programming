6c115ace448b5aee6c46776d9c8e1637fa60a967
darbumas <2314@holbertonschool.com>
2022-02-24 23:32:07 +0000

    task #8

---
+++ 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Uses ORM sqlalchemy to print the first State object from db hbtn_0e_6_usa"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+def main():
+    '''should not execute when imported'''
+
+    engine = create_engine(
+            f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}",
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).first()
+    if state:
+        print(f'{state.id}: {state.name}')
+    else:
+        print('Nothing')
+    session.close()
+
+if __name__ == "__main__":
+    main()
