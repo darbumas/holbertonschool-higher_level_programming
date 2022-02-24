b7760376ef90edd862c25382b71de9e56ad9dee5
darbumas <2314@holbertonschool.com>
2022-02-24 23:13:36 +0000

    use metadata.create_all to check for table presence

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Uses ORM sqlalchemy to all State objects from db hbtn_0e_6_usa"""
+"""Uses ORM sqlalchemy to list all State objects from db hbtn_0e_6_usa"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -12,6 +12,7 @@ def main():
     engine = create_engine(
             f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}",
             pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State).order_by(State.id).all():
