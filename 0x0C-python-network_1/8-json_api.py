a8a901f39c7a9dbc9f72cc286fd83abd63385b66
darbumas <2314@holbertonschool.com>
2022-03-08 20:59:51 +0000

    task #8

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        inpt = {'q': argv[1]}
+    else:
+        inpt = {'q': ""}
+    resp = requests.post(url, inpt)
+    try:
+        obj = r.json()
+        if len(obj) > 0:
+            print("[{}] {}".format(obj['id'], obj['name']))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
+
