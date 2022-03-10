c2555bdbdaed4a9387d98747252574e16925255c
darbumas <2314@holbertonschool.com>
2022-03-10 04:24:25 +0000

    print step by step

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -4,7 +4,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        response = res.read()
-    print('Body Response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
-          .format(type(response), response, response.decode('utf-8')))
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        body = response.read()
+
+        print("Body response:")
+        print("\t- type:{}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("UTF-8")))
