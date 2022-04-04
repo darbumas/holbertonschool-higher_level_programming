126e9a3b57fc90c442e78998cf45d95ec2333012
darbumas <2314@holbertonschool.com>
2022-04-04 19:49:19 +0000

    task #6

---
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Prints 3 lines using an array of strings and a loop
+
+const strArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i <= 2) {
+  console.log(strArr[i]);
+  i++;
+}
