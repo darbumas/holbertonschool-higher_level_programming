2c894bc458cba4503049dd4034bcfd95b952d532
darbumas <2314@holbertonschool.com>
2022-04-11 16:42:20 +0000

    task #5

---
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const req = require('request');
+const fs = require('fs');
+
+req(process.argv[2], function (err, response, body) {
+  if (err) throw err;
+  fs.writeFile(process.argv[3], body, 'utf8', function (err, data) {
+    if (err) throw err;
+  });
+});
